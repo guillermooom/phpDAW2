aaacf153a175db8ba73f139c7fb3edb6fbcdaed6
guillermoom <97970403+guillermooom@users.noreply.github.com>
2022-09-23 09:00:56 +0200

    Add files via upload

--- ut1/eje2.php
+++ ut1/eje2.php
@@ -11,26 +11,25 @@ $p3 =strpos($ip,".",($p2+1));
 
 //PRIMER OCTETO
 $oct1= substr($ip,0,$p1);	//SACMOS EL CONTENIDO DEL PRIMER OCTETO
-$bin1 = decbin($oct1);		//PASAMOS EL CONTENIDO DEL ANTERIOR A BINARIO
-$bin1 = substr("00000000",0,8 - strlen($bin1)) .$bin1;	//LE DAMOS FORMATO DE TODO 0 DESDE LA POSICION 0 HASTA LA 8 Y DEPUES LE PONEMOS EL RESULTADO DE NUEBO (X ESO ESTA AL FINAL .bin1)
 
 //SEGUNDO OCTETO
 $oct2= substr($ip,($p1+1),($p2-$p1));
-$bin2 = decbin($oct2);
-$bin2 = substr("00000000",0,8 - strlen($bin2)) . $bin2;
+
 
 //TERCER OCTETO
 $oct3= substr($ip,($p2+1),($p3-$p2));
-$bin3 = decbin($oct3);
-$bin3 = substr("00000000",0,8 - strlen($bin3)) . $bin3;
+
 
 //CUARTO OCTETO
 $oct4= substr($ip,($p3+1),$p3);
-$bin4 = decbin($oct4);
-$bin4 = substr("00000000",0,8 - strlen($bin4)) . $bin4;
 
-echo("Ip $ip en binario es: $bin1.$bin2.$bin3.$bin4 ");
 
+echo("Ip $ip en binario es: " .
+str_pad(decbin($oct1),8,"0",STR_PAD_LEFT).".".
+str_pad(decbin($oct2),8,"0",STR_PAD_LEFT).".".
+str_pad(decbin($oct3),8,"0",STR_PAD_LEFT).".".
+str_pad(decbin($oct4),8,"0",STR_PAD_LEFT));
 ?>
+
 </BODY>
 </HTML>
