ea28144812f439b2deba7421ef5b0cd2e57bd4fb
guillermoom <97970403+guillermooom@users.noreply.github.com>
2022-09-26 09:14:50 +0200

    Add files via upload

--- ut1/ej1b.php
+++ ut1/ej1b.php
@@ -2,17 +2,19 @@
 <HEAD><TITLE> EJ1B – Conversor decimal a binario</TITLE></HEAD>
 <BODY>
 <?php
-$num="168";
+$num="1";
 $n=$num;
 $res="";
 
-for($i=0;$i!=8;$i++){ //pongo 8 ya que una ip tiene 8 bits
+do{ //pongo 8 ya que una ip tiene 8 bits
 	$div=($n%2); //sacamos el resto de esta division
-	$n=$n/2;
+	$n=(int)($n/2);
 	$res="$div$res"; //primero le tenemos que dar el resultado para que se coleque al reves
-}
-echo "Numero $num en binario = $res";
+}while($n>$div);
 
+if($n!=0)$res="$n$res";
+
+echo "Numero $num en binario = $res";
 ?>
 </BODY>
 </HTML>
