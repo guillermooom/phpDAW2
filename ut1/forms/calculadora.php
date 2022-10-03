8c86ff0d449b0cc9331efa261fbc0290e9e9e28b
guillermoom <97970403+guillermooom@users.noreply.github.com>
2022-10-03 10:19:35 +0200

    Add files via upload

--- ut1/forms/calculadora.php
+++ ut1/forms/calculadora.php
@@ -17,41 +17,41 @@
 <?php
 $num1=$_POST["op1"];
 $num2=$_POST["op2"];
+$opera=$_POST["ope"];
 
-function sumar($n1,$n2){
+function ope($n1,$n2){
 	$resul=0;
+	$msg="";
+	
+	if($opera="suma"){
+		$resul=$n1+$n2;
+		$msg="<p>Resultado operación $n1 + $n2 = $resul</p>";
+	}
+	
+	if($opera="resta"){
+		$resul=$n1-$n2;
+		$msg="<p>Resultado operación $n1 - $n2 = $resul</p>";
+	}
+	
+	if($opera="multi"){
+		$resul=$n1*$n2;
+		$resul=round($resul,2);
+		$msg="<p>Resultado operación $n1 * $n2 = $resul</p>";
+	}
+	
+	if($opera="division"){
+		if($opera="division" && ($n1==0 || $n2==0)){
+			$msg="<p>ERROR NO PODUEDES DIVIDIR ENTRE 0</p>";
+		}else{
+			$resul=$n1/$n2;
+			$msg="<p>Resultado operación $n1 / $n2 = $resul</p>";
+		}
+	}
 
-	$resul=$n1+$n2;
-
-	return "<p>Resultado operación $n1 + $n2 = $resul</p>";
+	return $msg;
 }
 
-function restar($n1,$n2){
-	$resul=0;
-
-	$resul=$n1-$n2;
-
-	return "<p>Resultado operación $n1 - $n2 = $resul</p>";
-}
-
-function multiplica($n1,$n2){
-	$resul=0;
-
-	$resul=$n1*$n2;
-
-	return "<p>Resultado operación $n1 * $n2 = $resul</p>";
-}
-
-function division($n1,$n2){
-	$resul=0;
-
-	$resul=$n1/$n2;
-
-	return "<p>Resultado operación $n1 / $n2 = $resul</p>";
-}
-
-
-echo sumar($num1,$num2);
+echo ope($num1,$num2);
 ?>
 </BODY>
 </HTML>
