19816cf224fa2894434884eab3a23f56b37b5619
guillermooom <97970403+guillermooom@users.noreply.github.com>
2022-11-23 10:20:37 +0100

    Add files via upload

---
+++ ut1/empleadosnm/alta_dpto.php
@@ -0,0 +1,81 @@
+<HTML>
+<HEAD><TITLE>LISTADO EMPLEADOS</TITLE></HEAD>
+<style>
+h3{
+	text-align:center;
+}
+</style>
+<BODY>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "rootroot";
+$dbname = "empleadosnm";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT emple.dni, emple.nombre, emple.salario, emple.fecha_nac
+	FROM emple, dpto
+	WHERE dpto.cod_dpto=emple.cod_dpto
+	AND dpto.nombre=:nombredpto");
+	if ($_SERVER["REQUEST_METHOD"]== "POST"){
+		$valor = $_POST["dep"];//VALOR RECOGIDO POR LA PANTALLA
+	}
+	$stmt -> bindParam(':nombredpto',$valor);
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+	//el resultado SELECT en array asociativo resultado
+	$result = $stmt->fetchAll();
+	//var_dump($result);
+	foreach($result as $row) {
+        echo "Empleado: " . $row["dni"]. "-" . $row["nombre"]."-" . $row["salario"]."-" . $row["fecha_nac"]. "<br>";
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+
+?>
+<br>
+<h3>LISTADO EMPLEADOS </h3>
+
+<form name="formu" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+DPTO
+    <select name="dep">
+        
+    </select>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "rootroot";
+        $dbname = "empleadosnm";
+        
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $v= $conn->prepare("SELECT COUNT(*) FROM DPTO");
+           $v->execute();
+           echo $v;
+            
+            
+        }catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+
+    ?>
+    <br>
+    <br>
+    <input type="submit" value="enviar" name="enviar" />
+    <input type="reset" value="borrar" name="enviar" />
+</form>
+
+
+</BODY>
+</HTML>
