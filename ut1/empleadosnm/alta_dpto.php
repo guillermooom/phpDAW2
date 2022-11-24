75fd73f200949ab1ecf6035317385ac028e56137
guillermooom <97970403+guillermooom@users.noreply.github.com>
2022-11-24 12:12:44 +0100

    Add files via upload

--- ut1/empleadosnm/alta_dpto.php
+++ ut1/empleadosnm/alta_dpto.php
@@ -1,81 +1,66 @@
 <HTML>
-<HEAD><TITLE>LISTADO EMPLEADOS</TITLE></HEAD>
+<HEAD><TITLE>ALTA DPTO</TITLE></HEAD>
 <style>
 h3{
 	text-align:center;
 }
 </style>
 <BODY>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "rootroot";
-$dbname = "empleadosnm";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $stmt = $conn->prepare("SELECT emple.dni, emple.nombre, emple.salario, emple.fecha_nac
-	FROM emple, dpto
-	WHERE dpto.cod_dpto=emple.cod_dpto
-	AND dpto.nombre=:nombredpto");
-	if ($_SERVER["REQUEST_METHOD"]== "POST"){
-		$valor = $_POST["dep"];//VALOR RECOGIDO POR LA PANTALLA
-	}
-	$stmt -> bindParam(':nombredpto',$valor);
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-	//el resultado SELECT en array asociativo resultado
-	$result = $stmt->fetchAll();
-	//var_dump($result);
-	foreach($result as $row) {
-        echo "Empleado: " . $row["dni"]. "-" . $row["nombre"]."-" . $row["salario"]."-" . $row["fecha_nac"]. "<br>";
-    }
-}
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-$conn = null;
-
-?>
+<a href="empleadosnm.php">HOME</a>
 <br>
-<h3>LISTADO EMPLEADOS </h3>
-
+<h3>ALTA DPTO </h3>
 <form name="formu" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-DPTO
-    <select name="dep">
-        
-    </select>
-    <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "rootroot";
-        $dbname = "empleadosnm";
-        
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            $v= $conn->prepare("SELECT COUNT(*) FROM DPTO");
-           $v->execute();
-           echo $v;
-            
-            
-        }catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-        $conn = null;
-
-    ?>
-    <br>
-    <br>
-    <input type="submit" value="enviar" name="enviar" />
-    <input type="reset" value="borrar" name="enviar" />
+    <label type="text" name="nbDpto">NB Dpto <input name="nbDpto"></label>
+    <br><br>
+    <input type="submit" value="Añadir" name="enviar" />
 </form>
+<?php
+if(empty($_POST)){}
+else if($_POST["nbDpto"]!=""){
+    $servername = "localhost";
+    $username = "root";
+    $password = "rootroot";
+    $dbname = "empleadosnm";
 
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        $stmt = $conn->prepare("SELECT count(nombre) FROM dpto");
+        $stmt->execute();
+
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        //el resultado SELECT en array asociativo resultado
+        $result = $stmt->fetchAll();
+        foreach($result as $row) {
+            $contDpto = $row["count(nombre)"];
+        }
+
+        $contDpto++;
+        $contDpto=str_pad($contDpto,3,"0",STR_PAD_LEFT);
+        $codiDpto="D".$contDpto;
+
+        
+        $nuev = $conn->prepare("INSERT INTO DPTO (COD_DPTO,NOMBRE) VALUES(:codDp,:nom);");
+        $nuev->execute();
+
+        if ($_SERVER["REQUEST_METHOD"]== "POST"){
+            $nombr = $_POST["nbDpto"];
+        }
+
+        $nombr="Prueba";
+        $codiDpto="D009";
+        $nuev -> bindParam(':nom',$nombr);
+        $nuev -> bindParam(':codDp',$codiDpto);
+        
+
+    }
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+}
+?>
 </BODY>
 </HTML>
